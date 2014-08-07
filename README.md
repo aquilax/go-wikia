b449a1f0510d963c9450fdf9875175b4600e9d07
aquilax <evgeniy@wikia-inc.com>
2014-08-07 13:23:48 +0200

    Added activity test and godoc badge

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-go-wikia [![Build Status](https://travis-ci.org/aquilax/go-wikia.svg?branch=master)](https://travis-ci.org/aquilax/go-wikia)
+go-wikia [![Build Status](https://travis-ci.org/aquilax/go-wikia.svg?branch=master)](https://travis-ci.org/aquilax/go-wikia) [![GoDoc](https://godoc.org/github.com/aquilax/go-wikia?status.svg)](https://godoc.org/github.com/aquilax/go-wikia)
 ========
 
 Go library to access Wikia's public API
---
+++ activity_test.go
@@ -0,0 +1,25 @@
+package gowikia
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type parseLatesActivityTestCase struct {
+	blob []byte
+}
+
+func TestParseLatesActivity(t *testing.T) {
+	testCases := []parseLatesActivityTestCase{
+		{[]byte(`{"items":[{"article":1,"user":2,"revisionId":3,"timestamp":4}],"basepath":"http://example.com"}`)},
+		{[]byte(`{"items":[{"article":1,"user":2,"revisionId":3,"timestamp":4},{"article":5,"user":6,"revisionId":7,"timestamp":8}],"basepath":"http://example.com"}`)},
+	}
+	for i, testCase := range testCases {
+		lastestActivity, _ := parseLatesActivity(testCase.blob)
+		result, _ := json.Marshal(lastestActivity)
+		if string(testCase.blob) != string(result) {
+			t.Errorf("For testCase %d expected LatestActivityResult=%s but got LatestActivityResult=%s",
+				i, testCase.blob, result)
+		}
+	}
+}
