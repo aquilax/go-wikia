2638d0ed545e4a761531f4ff48be1ddd4aa906d8
aquilax <evgeniy@wikia-inc.com>
2014-08-08 11:19:48 +0200

    Restructooring; Added constructor test

--- wikia_test.go
+++ wikia_test.go
@@ -1,1 +1,16 @@
 package gowikia
+
+import (
+	"testing"
+)
+
+func TestNewWikiaApi(t *testing.T) {
+	mwa, _ := NewWikiaApi("")
+	if mwa != nil {
+		t.Errorf("Expected NewWikiaApi to fail return nil for invalid url")
+	}
+	_, err := NewWikiaApi("http://example.com")
+	if err != nil {
+		t.Errorf("Expected NewWikiaApi to succeed for valid url")
+	}
+}
