36a1b8082f8acc7c2d61419a8336fa1a6396ddf8
aquilax <aquilax@gmail.com>
2014-08-10 06:38:06 +0200

    Added search param constants

--- module_search.go
+++ module_search.go
@@ -4,6 +4,23 @@ import (
 	"encoding/json"
 )
 
+type SearchType string
+type SearchRank string
+
+const (
+	SEARCH_TYPE_ARTICLE SearchType = "article"
+	SEARCH_TYPE_VIDEO   SearchType = "video"
+
+	SEARCH_RANK_DEFAULT           SearchRank = "default"
+	SEARCH_RANK_NEWEST            SearchRank = "newest"
+	SEARCH_RANK_OLDEST            SearchRank = "oldest"
+	SEARCH_RANK_RECENTLY_MODIFIED SearchRank = "recently-modified"
+	SEARCH_RANK_STABLE            SearchRank = "stable"
+	SEARCH_RANK_MOST_VIEWED       SearchRank = "most-viewed"
+	SEARCH_RANK_FRESHEST          SearchRank = "freshest"
+	SEARCH_RANK_STALEST           SearchRank = "stalest"
+)
+
 type SearchListItem struct {
 	Id      int    `json:"id"`
 	Title   string `json:"title"`
@@ -22,8 +39,8 @@ type SearchListResult struct {
 
 type SearchListParams struct {
 	Query             string
-	Type              string
-	Rank              string
+	Type              SearchType
+	Rank              SearchRank
 	Limit             int
 	MinArticleQuality int
 	Batch             int
@@ -32,7 +49,15 @@ type SearchListParams struct {
 
 // Default request for Search List
 func DefaultSearchListParams() SearchListParams {
-	return SearchListParams{"test", "", "", 25, 10, 0, []int{0}}
+	return SearchListParams{
+		"test",
+		SEARCH_TYPE_ARTICLE,
+		SEARCH_RANK_DEFAULT,
+		25,
+		10,
+		0,
+		[]int{0},
+	}
 }
 
 // Do search for given phrase
@@ -43,8 +68,8 @@ func (wa *WikiaApi) SearchList(params SearchListParams) (*SearchListResult, erro
 		[]string{SEARCH_SEGMENT, SEARCH_LIST_SEGMENT},
 		RequestParams{
 			"query":             params.Query,
-			"type":              params.Type,
-			"rank":              params.Rank,
+			"type":              string(params.Type),
+			"rank":              string(params.Rank),
 			"limit":             intToStr(params.Limit),
 			"minArticleQuality": intToStr(params.MinArticleQuality),
 			"batch":             intToStr(params.Batch),
